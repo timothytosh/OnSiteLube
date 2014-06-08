0d397db6aa95f24343022f52feda96471ad4ffe3
timothytosh <tim.tosh@petroskills.com>
2014-06-07 23:30:50 -0500

    update for service schedule

--- src/OnSiteLube/AdminBundle/Entity/ServiceSchedule.php
+++ src/OnSiteLube/AdminBundle/Entity/ServiceSchedule.php
@@ -21,4 +21,83 @@ class ServiceSchedule {
    */
   private $id;
 
+  /**
+   * @var string
+   *
+   * @ORM\Column(name="Name", type="string", length=45)
+   */
+  private $name;
+
+  /**
+   * @var integer
+   *
+   * @ORM\Column(name="OilChgFilterMiles", type="integer")
+   */
+  private $oilChgFilterMiles;
+
+  /**
+   * @var integer
+   *
+   * @ORM\Column(name="OilChgFilterHours", type="integer")
+   */
+  private $oilChgFilterHours;
+
+  /**
+   * @var integer
+   *
+   * @ORM\Column(name="OilChgFilterMonths", type="integer")
+   */
+  private $oilChgFilterMonths;
+
+  /**
+   * @var integer
+   *
+   * @ORM\Column(name="FuelFilterMiles", type="integer")
+   */
+  private $fuelFilterMiles;
+
+  /**
+   * @var integer
+   *
+   * @ORM\Column(name="FuelFilterHours", type="integer")
+   */
+  private $fuelFilterHours;
+
+  /**
+   * @var integer
+   *
+   * @ORM\Column(name="AirFilterMiles", type="integer")
+   */
+  private $airFilterMiles;
+
+  /**
+   * @var integer
+   *
+   * @ORM\Column(name="AirFilterHours", type="integer")
+   */
+  private $airFilterHours;
+
+  /**
+   * @var integer
+   *
+   * @ORM\Column(name="TransFilterMiles", type="integer")
+   */
+  private $transFilterMiles;
+
+  /**
+   * @var integer
+   *
+   * @ORM\Column(name="TransFilterHours", type="integer")
+   */
+  private $transFilterHours;
+
+  /**
+   * @var integer
+   *
+   * @ORM\Column(name="TransFilterMonths", type="integer")
+   */
+  private $transFilterMonths;
+
+
+
 } 
